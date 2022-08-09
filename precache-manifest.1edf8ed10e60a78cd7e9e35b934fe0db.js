5574feb1a7e4827148a7e6ec91feb89eed12e759
Kenneth Fabian <fabiankenneth92@gmail.com>
2022-08-09 21:10:29 +0100

    Updates

---
+++ precache-manifest.1edf8ed10e60a78cd7e9e35b934fe0db.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "20c9a3757e5da4bf6c70",
+    "url": "/robo-friends/static/css/main.abe2fd6d.chunk.css"
+  },
+  {
+    "revision": "20c9a3757e5da4bf6c70",
+    "url": "/robo-friends/static/js/main.c2e3e04d.chunk.js"
+  },
+  {
+    "revision": "e26fa2088cf320dc503e",
+    "url": "/robo-friends/static/js/runtime~main.04d6e5ad.js"
+  },
+  {
+    "revision": "c2dd0463dba1d0825430",
+    "url": "/robo-friends/static/css/2.168bd9a1.chunk.css"
+  },
+  {
+    "revision": "c2dd0463dba1d0825430",
+    "url": "/robo-friends/static/js/2.c39f23a7.chunk.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robo-friends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "876a67a0fef5738ea21681e2b1999382",
+    "url": "/robo-friends/index.html"
+  }
+];
